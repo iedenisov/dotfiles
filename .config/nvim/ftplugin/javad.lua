651a95f81bea2eeb47faed1846cea07e1479b070
Ivan Denisov <denisov.ndg@gmail.com>
2025-03-24 18:58:08 +0500

    neovim: setup JDTLS

---
+++ .config/nvim/ftplugin/javad.lua
@@ -0,0 +1,72 @@
+local home = os.getenv('HOME')
+
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local config = {
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    cmd = {
+        home .. "/.jbang/cache/jdks/21/bin/java",
+        "-javaagent:" .. home .. "/.local/share/eclipse/lombok.jar",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens", "java.base/java.util=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        '-jar', vim.fn.glob(home ..'/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+        "-configuration", home .. "/jdtls/config_linux",
+        "-data", workspace_folder,
+    },
+    root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew", "pom.xml"}),
+    settings = {
+        java = {
+            signatureHelp = {
+                enabled = true,
+            },
+            import = {
+                maven = {
+                    enabled = true,
+                },
+            },
+            -- contentProvider = { preferred = "fernflower" },
+            -- completion = {
+            --     signatureHelp = { enabled = true },
+            --     inlayHints = { enabled = true },
+            --     favoriteStaticMembers = {
+            --         -- 'com.sun.*',
+            --         -- 'io.micrometer.shaded.*',
+            --         -- 'jakarta.*',
+            --         -- 'java.awt.*',
+            --         -- 'jdk.*',
+            --         -- 'sun.*',
+            --         -- 'org.springframework.*',
+            --         -- 'org.mockito.Mockito.*',
+            --         -- 'lombok.*',
+            --         -- 'import com.atlassian.*',
+            --     },
+            --     -- filteredTypes = {
+            --     --     'com.sun.*',
+            --     --     'io.micrometer.shaded.*',
+            --     --     'jakarta.*',
+            --     --     'java.awt.*',
+            --     --     'jdk.*',
+            --     --     'sun.*',
+            --     --     'org.springframework.*',
+            --     --     'lombok.*',
+            --     --     'import com.atlassian.*',
+            --     -- }
+            -- },
+            -- eclipse = {
+            --     downloadSources = true,
+            -- },
+            -- maven = {
+            --     downloadSources = true,
+            -- },
+        }
+    }
+}
+
+require('jdtls').start_or_attach(config)
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,29 +4,14 @@ return {
     config = function()
         local home = os.getenv('HOME')
         local lspconfig = require('lspconfig')
-        -- lspconfig.lua_ls.setup{
-        --     settings = {
-        --         Lua = {
-        --             hint = {
-        --                 enable = true,
-        --             },
-        --         },
-        --         runtime = {
-        --             version = 'LuaJIT',
-        --         },
-        --         workspace = {
-        --             checkThisParty = true,
-        --             library = {
-        --                 vim.env.VIMRUNTIME,
-        --                 '~/.local/share/nvim/lazy/solarized.nvim',
-        --             },
-        --         },
-        --     },
-        -- }
 
-        lspconfig.cssls.setup{}
-        lspconfig.html.setup{}
-        -- lspconfig.jdtls.setup{
+        -- JDTLS
+        local root_markers = {'gradlew', 'mvnw', '.git', 'pom.xml'}
+        local root_dir = require('jdtls.setup').find_root(root_markers)
+        local workspace_dir = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+        lspconfig.jdtls.setup{
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
             -- cmd = {
             --     home .. "/.jbang/cache/jdks/21/bin/java",
             --     "-javaagent:" .. home .. "/.local/share/eclipse/lombok.jar",
@@ -43,63 +28,63 @@ return {
             --     "-configuration", home .. "/jdtls/config_linux",
             --     "-data", workspace_dir,
             -- },
+            root_dir = root_dir,
+            settings = {
+                java = {
+                    contentProvider = { preferred = "fernflower" },
+                    references = {
+                        includeDecompiledSources = true,
+                    },
+                    sources = {
+                        organizeImports = {
+                            starThreshold = 9999,
+                            staticStarThreshold = 9999,
+                        },
+                    },
+                    completion = {
+                        signatureHelp = { enabled = true },
+                        favoriteStaticMembers = {
+                            "org.springframework.*",
+                        },
+                        codeGeneration = {
+                            toString = {
+                                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+                            },
+                            useBlocks = true,
+                        },
+                        -- eclipse {
+                        --     downloadSources = true
+                        -- },
+                        -- maven {
+                        --     downloadSources = true
+                        -- },
+                        configuration = {
+                            runtimes = {
+                                {
+                                    name = "JavaSE-1.8",
+                                    path = home .. "/.jbang/cache/jdks/8"
+                                },
+                                {
+                                    name = "JavaSE-11",
+                                    path = home .. "/.jbang/cache/jdks/11"
+                                },
+                                {
+                                    name = "JavaSE-17",
+                                    path = home .. "/.jbang/cache/jdks/17"
+                                },
+                                {
+                                    name = "JavaSE-21",
+                                    path = home .. "/.jbang/cache/jdks/21"
+                                },
+                            }
+                        }
+                    },
+                },
+            },
+        }
 
-            -- settings = {
-            --     java = {
-            --         extendedClientCapabilities = {
-            --             resolveAdditionalTextEditsSupport = true
-            --         },
-            --         contentProvider = { preferred = "fernflower" },
-            --         references = {
-            --             includeDecompiledSources = true,
-            --         },
-            --         sources = {
-            --             organizeImports = {
-            --                 starThreshold = 9999,
-            --                 staticStarThreshold = 9999,
-            --             },
-            --         },
-            --         completion = {
-            --             signatureHelp = { enabled = true },
-            --             favoriteStaticMembers = {
-            --                 "org.springframework.*",
-            --             },
-            --             codeGeneration = {
-            --                 toString = {
-            --                     template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-            --                 },
-            --                 useBlocks = true,
-            --             },
-            --             -- eclipse {
-            --             --     downloadSources = true
-            --             -- },
-            --             -- maven {
-            --             --     downloadSources = true
-            --             -- },
-            --             configuration = {
-            --                 -- runtimes = {
-            --                 --     {
-            --                 --         name = "JavaSE-1.8",
-            --                 --         path = "/home/ivdenisov/.jbang/cache/jdks/8"
-            --                 --     },
-            --                 --     {
-            --                 --         name = "JavaSE-11",
-            --                 --         path = "/home/ivdenisov/.jbang/cache/jdks/11"
-            --                 --     },
-            --                 --     {
-            --                 --         name = "JavaSE-17",
-            --                 --         path = "/home/ivdenisov/.jbang/cache/jdks/17"
-            --                 --     },
-            --                 --     {
-            --                 --         name = "JavaSE-21",
-            --                 --         path = "/home/ivdenisov/.jbang/cache/jdks/21"
-            --                 --     },
-            --                 -- }
-            --             }
-            --         },
-            --     },
-            -- },
-        -- }
+        lspconfig.cssls.setup{}
+        lspconfig.html.setup{}
         lspconfig.lua_ls.setup{}
         lspconfig.pyright.setup{}
         lspconfig.markdown_oxide.setup{}
